11f07fa48baef9a4d6d259ad1aae58ca2293c5f2
naturedevs <naturesky0411@gmail.com>
2024-05-24 07:38:32 -0700

    1

--- src/components/all-you-need-section.jsx
+++ src/components/all-you-need-section.jsx
@@ -10,7 +10,7 @@ export default function AllYouNeed() {
       </div>
       <div className="container pb-20 lg:px-0">
         <div className="hidden lg:flex blocks-row text-center lg:justify-between lg:pt-20 xl:justify-center scaleup lg:max-w-[1020px] m-auto lg:gap-[10px] flex-wrap">
-          <div className='block block-1  z-10w-[33.33%] lg:w-[160px] mb-[30px] lg:mb-[50px] relative' data-connect='block-3, block-7, block-8'>
+          <div className='z-[50] hover:z-0 block block-1  w-[33.33%] lg:w-[160px] mb-[30px] lg:mb-[50px] relative' data-connect='block-3, block-7, block-8'>
             <div className='block-inner'>
               <div className='absolute top-[40px] left-[100px] w-[298px] rotate-[0deg] bg-gradient-to-r from-[#5BBA6F] to-[#289590] h-[1px] z-20aa'></div>
               <div className='absolute top-[132px] left-[82px] w-[420px] rotate-[22deg] bg-gradient-to-r from-[#5BBA6F] to-[#FFBF00] h-[1px] z-20aa'></div>
@@ -27,7 +27,7 @@ export default function AllYouNeed() {
               <span className='t-heading-6-sm text-[#181819]'>Point of Sale</span>
             </div>
           </div>
-          <div className='block block-2  z-10w-[33.33%] lg:w-[160px] mb-[30px] lg:mb-[50px] relative' data-connect='block-1, block-7, block-8, block-9, block-10'>
+          <div className='z-[50] hover:z-0 block block-2  w-[33.33%] lg:w-[160px] mb-[30px] lg:mb-[50px] relative' data-connect='block-1, block-7, block-8, block-9, block-10'>
             <div className='block-inner'>
               <div className='absolute top-[40px] right-[102px] w-[132px] rotate-[180deg] bg-gradient-to-r from-[#ED254E] to-[#5BBA6F] h-[1px] z-20aa'></div>
               <div className='absolute top-[130px] xl:hidden right-[60px] w-[210px] rotate-[130deg] bg-gradient-to-r from-[#ED254E] to-[#06AED5] h-[1px] z-20aa'></div>
@@ -47,7 +47,7 @@ export default function AllYouNeed() {
               <span className='t-heading-6-sm text-[#181819]'>Marketing<br />& Loyalty</span>
             </div>
           </div>
-          <div className='block block-3  z-10w-[33.33%] lg:w-[160px] mb-[30px] lg:mb-[50px] relative' data-connect='block-1, block-9, block-10'>
+          <div className='z-[50] hover:z-0 block block-3  w-[33.33%] lg:w-[160px] mb-[30px] lg:mb-[50px] relative' data-connect='block-1, block-9, block-10'>
             <div className='block-inner'>
               <div className='absolute top-[40px] right-[106px] w-[298px] rotate-[180deg] bg-gradient-to-r from-[#289590] to-[#5BBA6F] h-[1px] z-20aa'></div>
               <div className='absolute top-[134px] xl:hidden left-[-80px] w-[180px] rotate-[120deg] bg-gradient-to-r from-[#289590] to-[#43C798] h-[1px] z-20aa'></div>
@@ -65,7 +65,7 @@ export default function AllYouNeed() {
               <span className='t-heading-6-sm text-[#181819]'>Integrated<br />Payments</span>
             </div>
           </div>
-          <div className='block block-4  z-10w-[33.33%] lg:w-[160px] mb-[30px] lg:mb-[50px] relative' data-connect='block-7, block-8, block-9'>
+          <div className='z-[50] hover:z-0 block block-4  w-[33.33%] lg:w-[160px] mb-[30px] lg:mb-[50px] relative' data-connect='block-7, block-8, block-9'>
             <div className='block-inner'>
               <div className='absolute top-[130px] xl:hidden right-[80px] w-[510px] rotate-[160deg] bg-gradient-to-r from-[#A682FF] to-[#06AED5] h-[1px] z-20aa'></div>
               <div className='absolute top-[135px] lg:hidden xl:block right-[78px] w-[462px] rotate-[156deg] bg-gradient-to-r from-[#A682FF] to-[#06AED5] h-[1px] z-20aa'></div>
@@ -83,7 +83,7 @@ export default function AllYouNeed() {
               <span className='t-heading-6-sm text-[#181819]'>Delivery</span>
             </div>
           </div>
-          <div className='block block-5  z-10w-[33.33%] lg:w-[160px] mb-[30px] lg:mb-[50px] relative' data-connect='block-1, block-6, block-8, block-9, block-10'>
+          <div className='z-[50] hover:z-0 block block-5  w-[33.33%] lg:w-[160px] mb-[30px] lg:mb-[50px] relative' data-connect='block-1, block-6, block-8, block-9, block-10'>
             <div className='block-inner'>
               <div className='absolute top-[40px] right-[102px] w-[650px] rotate-[180deg] bg-gradient-to-r from-[#F67227] to-[#5BBA6F] h-[1px] z-20aa'></div>
               <div className='absolute top-[130px] right-[71px] w-[270px] rotate-[145deg] bg-gradient-to-r from-[#F67227] to-[#FFBF00] h-[1px] z-20aa'></div>
@@ -104,7 +104,7 @@ export default function AllYouNeed() {
               <span className='t-heading-6-sm text-[#181819]'>Inventory<br />Management</span>
             </div>
           </div>
-          <div className='block block-6  z-10w-[33.33%] lg:w-[160px] mb-[30px] lg:mb-[50px] relative' data-connect='block-5'>
+          <div className='z-[50] hover:z-0 block block-6  w-[33.33%] lg:w-[160px] mb-[30px] lg:mb-[50px] relative' data-connect='block-5'>
             <div className='block-inner'>
               <div className='absolute top-[40px] right-[101px] w-[129px] rotate-[180deg] bg-gradient-to-r from-[#B84FD7] to-[#F67227] h-[1px] z-20aa'></div>
               <div className='absolute top-[132px] right-[0px] w-[166px] rotate-[90deg] xl:hidden bg-gradient-to-r from-[#B84FD7] to-[#D74FA9] h-[1px] z-20aa'></div>
@@ -120,7 +120,7 @@ export default function AllYouNeed() {
               <span className='t-heading-6-sm text-[#181819]'>Integrations</span>
             </div>
           </div>
-          <div className='block block-7  z-10w-[33.33%] lg:w-[160px] custom-w1 mb-[30px] lg:mb-[50px] relative' data-connect='block-2,block-8,block-10, block-4, block-9'>
+          <div className='z-[50] hover:z-0 block block-7  w-[33.33%] lg:w-[160px] custom-w1 mb-[30px] lg:mb-[50px] relative' data-connect='block-2,block-8,block-10, block-4, block-9'>
             <div className='block-inner'>
               <div className='absolute bottom-[88px] left-[310px] w-[180px] rotate-[0deg] xl:hidden bg-gradient-to-r from-[#06AED5] to-[#FFBF00] h-[1px] z-20aa'></div>
               <div className='absolute bottom-[88px] left-[260px] w-[180px] rotate-[0deg] lg:hidden xl:block bg-gradient-to-r from-[#06AED5] to-[#FFBF00] h-[1px] z-20aa'></div>
@@ -146,7 +146,7 @@ export default function AllYouNeed() {
               <span className='t-heading-6-sm text-[#181819]'>Customers</span>
             </div>
           </div>
-          <div className='block block-9  z-10w-[33.33%] lg:w-[160px] custom-w1 mb-[30px] lg:mb-[50px] relative' data-connect='block-1,block-2, block-4, block-8, block-7, block-10'>
+          <div className='z-[50] hover:z-0 block block-9  w-[33.33%] lg:w-[160px] custom-w1 mb-[30px] lg:mb-[50px] relative' data-connect='block-1,block-2, block-4, block-8, block-7, block-10'>
             <div className='block-inner'>
               <div className='absolute bottom-[88px] left-[300px] w-[190px] xl:hidden rotate-[180deg] bg-gradient-to-r from-[#5076D8] to-[#FFBF00] h-[1px] z-20aa'></div>
               <div className='absolute bottom-[88px] right-[94px] w-[190px] xl:hidden rotate-[0deg] bg-gradient-to-r from-[#06AED5] to-[#43C798] h-[1px] z-20aa'></div>
@@ -172,7 +172,7 @@ export default function AllYouNeed() {
               <span className='t-heading-6-sm text-[#181819]'>Ecommerce</span>
             </div>
           </div>
-          <div className='block block-8  z-10w-[33.33%] lg:w-[160px] custom-w1 mb-[30px] lg:mb-[50px] relative' data-connect='block-1, block-2, block-4,block-7, block-9, block-10'>
+          <div className='z-[50] hover:z-0 block block-8  w-[33.33%] lg:w-[160px] custom-w1 mb-[30px] lg:mb-[50px] relative' data-connect='block-1, block-2, block-4,block-7, block-9, block-10'>
             <div className='block-inner'>
               <div className='absolute bottom-[88px] right-[300px] w-[190px] rotate-[0deg] bg-gradient-to-r xl:hidden from-[#06AED5] to-[#43C798] h-[1px] z-20aa'></div>
               <div className='absolute bottom-[88px] right-[103px] w-[180px] rotate-[0deg] bg-gradient-to-r xl:hidden from-[#06AED5] to-[#FFBF00] h-[1px] z-20aa'></div>
@@ -198,7 +198,7 @@ export default function AllYouNeed() {
               <span className='t-heading-6-sm text-[#181819]'>Analytics</span>
             </div>
           </div>
-          <div className='block block-10 z-10 w-[33.33%] lg:w-[160px] custom-w1 mb-[30px] lg:mb-[50px] relative' data-connect='block-3, block-5, block-8'>
+          <div className='z-[50] hover:z-0 block block-10 w-[33.33%] lg:w-[160px] custom-w1 mb-[30px] lg:mb-[50px] relative' data-connect='block-3, block-5, block-8'>
             <div className='block-inner'>
               <div className='absolute bottom-[88px] right-[313px] w-[180px] rotate-[0deg] bg-gradient-to-r xl:hidden from-[#06AED5] to-[#FFBF00] h-[1px] z-20aa'></div>
               <div className='absolute bottom-[88px] right-[520px] w-[190px] rotate-[0deg] bg-gradient-to-r xl:hidden from-[#06AED5] to-[#43C798] h-[1px] z-20aa'></div>
@@ -222,7 +222,7 @@ export default function AllYouNeed() {
               <span className='t-heading-6-sm text-[#181819]'>In-Store Screens</span>
             </div>
           </div>
-          <div className='block block-11 w-[33.33%] lg:w-[160px] custom-w1 mb-[30px] lg:mb-[50px] relative' data-connect='block-6'>
+          <div className='z-[50] hover:z-0 block block-11 w-[33.33%] lg:w-[160px] custom-w1 mb-[30px] lg:mb-[50px] relative' data-connect='block-6'>
             <div className='block-inner'>
               <div className='absolute bottom-[175px] left-[0px] w-[166px] rotate-[90deg] bg-gradient-to-r xl:hidden from-[#B84FD7] to-[#D74FA9] h-[1px] z-20aa'></div>
               <div className='absolute bottom-[180px] left-[30px] w-[176px] rotate-[115deg] bg-gradient-to-r lg:hidden xl:block from-[#B84FD7] to-[#D74FA9] h-[1px] z-20aa'></div>

876f577dc89dec9871f75386527b90ad54761943
codelover <wmeng0103@gmail.com>
2024-06-25 14:17:39 +0200

    1

--- src/components/banner-section.jsx
+++ src/components/banner-section.jsx
@@ -1,15 +1,20 @@
 'use client'
 import Image from 'next/image'
 import Link from "next/link"
+import { useState } from 'react'
 
 export default function Banner(props) {
+  const [opacity, setOpacity] = useState(0)
+  setTimeout(() => {
+    setOpacity(1)
+  },2000)
   return (
     <>
-    <div className={` bg-cover justify-center bg-[url('/images/homepage/home_banner_bg.jpg')] relative w-full flex items-center xl:pt-[80px] ${props.class} overflow-hidden`}>
+    <div className={` bg-cover justify-center bg-[url('/images/homepage/home_banner_bg1.jpg')] relative w-full flex items-center xl:pt-[80px] ${props.class} overflow-hidden`}>
       <spline-viewer url="/videos/1.spline" 
       // <spline-viewer url="https://prod.spline.design/OM1pqrY7nYNVq365/scene.splinecode" 
-        style={{height: "calc(100% + 200px)"}}
-        class={`justify-center  absolute w-full flex items-center xl:pt-[80px] ${props.class} overflow-hidden`} />
+        style={{height: "calc(100% + 200px)",opacity:opacity, transitionDuration:"10000ms"}}
+        class={`justify-center  absolute w-full transition-all flex items-center xl:pt-[80px] ${props.class} overflow-hidden`} />
 
         <div className="container pt-[120px] xl:pt-[50px] pb-[40px] lg:pb-[80px] flex items-center z-40">
 
